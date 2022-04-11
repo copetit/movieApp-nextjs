9e25b475f74182b87c82a63623db167c8a149001
han <copetit412@gmail.com>
2022-04-11 21:58:46 +0900

    add star svg

--- pages/movies/[...params].js
+++ pages/movies/[...params].js
@@ -47,7 +47,22 @@ export default function Detail({ params }) {
                 </div>
                 <div className="detail">
                   <div className="info">
-                    <div className="score">⭐️ {movie.vote_average}</div>
+                    <div className="score">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="#f1be1e"
+                        viewBox="0 0 24 24"
+                        stroke="#f1be1e"
+                        strokeWidth="1"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+                        />
+                      </svg>
+                      <span>{movie.vote_average}</span>
+                    </div>
                     <h1 className="title">{title}</h1>
                     <div className="release-date">
                       {movie.release_date.slice(0, 4)}
@@ -132,8 +147,15 @@ export default function Detail({ params }) {
             margin-bottom: 50px;
           }
           .score {
-            font-size: 1.5em;
-            text-align: right;
+            height: 50px;
+            font-size: 2em;
+            display: flex;
+            align-items: center;
+            justify-content: right;
+          }
+          .score > svg {
+            padding: 5px;
+            height: 100%;
           }
           .title {
             font-size: 2.5em;
