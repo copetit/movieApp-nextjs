4ed8dfb971b1c7be153a30e8b460a9f7843d0384
han <copetit412@gmail.com>
2022-04-11 20:14:31 +0900

    delete

--- pages/index.js
+++ pages/index.js
@@ -62,7 +62,6 @@ export default function Home({ results }) {
 }
 
 export async function getServerSideProps() {
-  console.log('fuga!')
   const { results } = await (
     await fetch("https://movie-app-nextjs-orpin.vercel.app/api/movies")
   ).json();
