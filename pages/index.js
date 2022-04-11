b637c7b7c8d60a513f52f78903f3c981fd9730ee
han <copetit412@gmail.com>
2022-04-11 20:08:23 +0900

    change url

--- pages/index.js
+++ pages/index.js
@@ -64,7 +64,7 @@ export default function Home({ results }) {
 export async function getServerSideProps() {
   console.log('fuga!')
   const { results } = await (
-    await fetch("http://localhost:3000/api/movies")
+    await fetch("https://movie-app-nextjs-orpin.vercel.app/api/movies")
   ).json();
   return {
     props: {
